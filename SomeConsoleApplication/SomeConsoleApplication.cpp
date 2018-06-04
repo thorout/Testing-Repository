7643a425a55ff739688647492c109fc1ebdc03a4
thorout <thorout@gmail.com>
2018-06-04 13:38:09 +0300

    Final changes

--- SomeConsoleApplication/SomeConsoleApplication.cpp
+++ SomeConsoleApplication/SomeConsoleApplication.cpp
@@ -8,6 +8,11 @@
 int main()
 {
 	std::cout << "Hello, world... ! Changes have added...\n";
+	for (size_t i = 0; i < 10; i++)
+	{
+		std::cout << "\t" << i;
+	}
+	std::cout << "\n";
     return 0;
 }
 
