cb588b645013a0d97476ed0e5f42f6c77f1d9560
thorout <thorout@gmail.com>
2018-06-04 13:39:52 +0300

    Revert "Add some loop"
    This reverts commit 2d609afbe1a077af486d7127a5a768d0f5f7c695.

--- SomeConsoleApplication/SomeConsoleApplication.cpp
+++ SomeConsoleApplication/SomeConsoleApplication.cpp
@@ -10,7 +10,7 @@ int main()
 	std::cout << "Hello, world... ! Changes have added...\n";
 	for (size_t i = 0; i < 10; i++)
 	{
-		std::cout << "\t" << i;
+		std::cout << " " << i;
 	}
 	std::cout << "\n";
     return 0;
